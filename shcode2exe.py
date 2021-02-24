fff1abeb4d6c4c22ebc7b53dfdcc5dbdcf1296ba
AccidentalRebel <accidentalrebel@gmail.com>
2021-02-24 14:52:50 +0800

    Can now compile and link.

--- shcode2exe.py
+++ shcode2exe.py
@@ -1,3 +1,8 @@
+from os import uname, system
+
+current_bit = '32'
+filename = 'output'
+
 ASM_FILE_NAME = 'shcode.asm'
 ASM_FILE_CONTENTS = '\tglobal _start\n' \
     '\tsection .text\n' \
@@ -6,3 +11,16 @@ ASM_FILE_CONTENTS = '\tglobal _start\n' \
 
 with open(ASM_FILE_NAME, 'w+') as f:
     f.write(ASM_FILE_CONTENTS)
+
+cmd = 'tools/nasm/nasm'
+if uname().sysname != "Linux":
+    cmd += '.exe'
+cmd += ' -f win' + current_bit + ' -o shcode.obj shcode.asm' 
+system(cmd)
+
+cmd = 'tools/linkers/ld' + current_bit
+if uname().sysname != "Linux":
+    cmd += '.exe'
+cmd += ' -o ' + filename + '.exe shcode.obj'
+system(cmd)
+
